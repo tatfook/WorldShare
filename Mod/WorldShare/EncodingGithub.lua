c1230597c0918feedd5adb1533a343317ded342f
onedou <onedou@126.com>
2017-01-10 15:50:05 +0800

    fixed bug
    Signed-off-by: onedou <onedou@126.com>

--- Mod/WorldShare/EncodingGithub.lua
+++ Mod/WorldShare/EncodingGithub.lua
@@ -14,13 +14,13 @@ NPL.load("(gl)script/ide/System/Encoding/base64.lua");
 local EncodingGithub = commonlib.inherit(nil,commonlib.gettable("Mod.WorldShare.EncodingGithub"));
 local EncodingS      = commonlib.gettable("System.Encoding");
 
--- =转成2个-  +转成3个-  /转成4个- 
+-- =转成-p1p  +转成-p2p  /转成-p3p
 function EncodingGithub.base64(text)
 	if(text) then
 		text = EncodingS.base64(text);
-		text = text:gsub("[=]" ,"--");
-		text = text:gsub("[%+]","---");
-		text = text:gsub("[/]" ,"----");
+		text = text:gsub("[=]" ,"-p1p");
+		text = text:gsub("[%+]","-p2p");
+		text = text:gsub("[/]" ,"-p3p");
 
 		--LOG.std(nil,"debug","text",text);
 
@@ -32,9 +32,9 @@ end
 
 function EncodingGithub.unbase64(text)
 	if(text) then
-		text = text:gsub("[--]","=");
-		text = text:gsub("[---]","+");
-		text = text:gsub("[----]","/");
+		text = text:gsub("[-p1p]","=");
+		text = text:gsub("[-p2p]","+");
+		text = text:gsub("[-p3p]","/");
 
 		return EncodingS.unbase64(text);
 	else
