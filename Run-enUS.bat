a78150e89039dada4302c2fc38652fef6bd744e5
big <onedou@126.com>
2017-11-13 11:44:17 +0800

    add enUS run bat

---
+++ Run-enUS.bat
@@ -0,0 +1,8 @@
+@echo off 
+if exist "%~dp0../../redist/ParaEngineClient.exe" (
+pushd "%~dp0../../redist/" 
+) else (
+pushd "%~dp0../../" 
+)
+call "ParaEngineClient.exe" single="false" mc="true" noupdate="true" dev="%~dp0" mod="WorldShare" isDevEnv="true"  language="enUS"
+popd 
