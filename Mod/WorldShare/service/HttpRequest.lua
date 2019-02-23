05b058966257a00742ec69a317ddbd022078fe1b
LXZ_svn_sync <lixizhi@yeah.net>
2019-02-23 11:24:20 +0800

    fix httprequest with duplicated ?

--- Mod/WorldShare/service/HttpRequest.lua
+++ Mod/WorldShare/service/HttpRequest.lua
@@ -41,18 +41,19 @@ function HttpRequest:GetUrl(params, callback, noTryStatus)
     end
 
     if (formatParams.method == "GET" and type(params) == "table") then
-        local url = format("%s?", params.url or "")
-        local paramsString = ""
-
-        for key, value in pairs(params.form or {}) do
-            if type(value) == 'string' or type(value) == 'number' then
-                paramsString = paramsString .. key .. "=" .. value .. "&"
+        if(params.form) then
+            local url = params.url;
+            local paramsString = ""
+            for key, value in pairs(params.form or {}) do
+                if type(value) == 'string' or type(value) == 'number' then
+                    paramsString = paramsString .. key .. "=" .. value .. "&"
+                end
+            end
+            paramsString = string.sub(paramsString, 1, -2)
+            if(paramsString and paramsString~="") then
+                formatParams.url = format("%s?%s", url, paramsString)
             end
         end
-
-        paramsString = string.sub(paramsString, 1, -2)
-
-        formatParams.url = format("%s%s", url, paramsString)
     end
 
     if (formatParams.method ~= "GET") then
