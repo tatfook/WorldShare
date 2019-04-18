44c0bb86b031ab7443d099591430592876febe2f
onedou <onedou@126.com>
2019-04-19 07:33:36 +0800

    fix msg box

--- Mod/WorldShare/cellar/Common/MsgBox.lua
+++ Mod/WorldShare/cellar/Common/MsgBox.lua
@@ -13,7 +13,14 @@ local Store = NPL.load("(gl)Mod/WorldShare/store/Store.lua")
 
 local MsgBox = NPL.export()
 
-function MsgBox:Show(msg)
+MsgBox.count = 0
+MsgBox.allMsgBox = commonlib.Array:new()
+
+function MsgBox:Show(msg, sec, overtimeMsg)
+    self.count = self.count + 1
+    local curIndex = self.count
+    self.allMsgBox:push_back(curIndex)
+
     Store:Set("user/msg", msg)
 
     local params = Utils:ShowWindow(0, 0, "Mod/WorldShare/cellar/Common/MsgBox.html", "MsgBox", 0, 0, "_fi", false)
@@ -22,18 +29,48 @@ function MsgBox:Show(msg)
         Store:Remove("page/MsgBox")
         Store:Remove("user/msg")
     end
-end
 
-function MsgBox:Close()
+    if not sec or type(sec) ~= 'number' then
+        return curIndex
+    end
+
     Utils.SetTimeOut(
         function()
-            local MessageInfoPage = Store:Get("page/MsgBox")
+            for key, item in ipairs(self.allMsgBox) do
+                if (item == curIndex) then
+                    _guihelper.MessageBox(overtimeMsg)
 
-            if (MessageInfoPage) then
-                MessageInfoPage:CloseWindow()
+                    self.allMsgBox:remove(key)
+                    local MessageInfoPage = Store:Get("page/MsgBox")
+
+                    if (MessageInfoPage) then
+                        MessageInfoPage:CloseWindow()
+                    end
+                end
+            end
+        end,
+        sec or 0
+    )
+
+    return curIndex
+end
+
+function MsgBox:Close(index)
+    if type(index) == 'number' then
+        for key, value in ipairs(self.allMsgBox) do
+            if value == index then
+                self.allMsgBox:remove(key)
             end
         end
-    )
+    else
+        self.allMsgBox:remove(#self.allMsgBox)
+    end
+
+    local MessageInfoPage = Store:Get("page/MsgBox")
+
+    if (MessageInfoPage) then
+        MessageInfoPage:CloseWindow()
+    end
 end
 
 function MsgBox.SetPage()
