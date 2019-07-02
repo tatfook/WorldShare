8c713a83a9b0f2b8018eea421974ccabd131f99e
onedou <onedou@126.com>
2019-07-02 10:37:22 +0800

    fix open world dir on share world dialog

--- Mod/WorldShare/main.lua
+++ Mod/WorldShare/main.lua
@@ -51,7 +51,7 @@ local WorldCommon = commonlib.gettable("MyCompany.Aries.Creator.WorldCommon")
 local WorldShare = commonlib.inherit(commonlib.gettable("Mod.ModBase"), commonlib.gettable("Mod.WorldShare"))
 
 WorldShare:Property({"Name", "WorldShare"})
-WorldShare.version = '0.0.4'
+WorldShare.version = '0.0.5'
 
 -- LOG.SetLogLevel("DEBUG");
 LOG.std(nil, "info", "WorldShare", "world share version %s", WorldShare.version)
