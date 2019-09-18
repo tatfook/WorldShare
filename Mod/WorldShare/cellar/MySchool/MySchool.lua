a7ede14740fb49e101eeaaca46657bce99721860
onedou <onedou@126.com>
2019-09-18 10:04:16 +0800

    1. add my school page 2. update user console page 3. update config env

---
+++ Mod/WorldShare/cellar/MySchool/MySchool.lua
@@ -0,0 +1,44 @@
+--[[
+Title: my school page
+Author(s):  big
+Date: 2019.09.11
+Desc: 
+use the lib:
+------------------------------------------------------------
+local MySchool = NPL.load("(gl)Mod/WorldShare/cellar/MySchool/MySchool.lua")
+------------------------------------------------------------
+]]
+
+local Utils = NPL.load("(gl)Mod/WorldShare/helper/Utils.lua")
+local Store = NPL.load("(gl)Mod/WorldShare/store/Store.lua")
+local KeepworkService = NPL.load("(gl)Mod/WorldShare/service/KeepworkService.lua")
+
+local MySchool = NPL.export()
+
+function MySchool:Show()
+    local params = Utils:ShowWindow(870, 650, "Mod/WorldShare/cellar/MySchool/MySchool.html", "MySchool")
+
+    params._page:CallMethod("nplbrowser_instance", "SetVisible", true)
+
+    params._page.OnClose = function()
+        Store:Remove('page/MySchoolPage')
+        params._page:CallMethod("nplbrowser_instance", "SetVisible", false)
+    end
+end
+
+function MySchool:SetPage()
+    Store:Set('page/MySchoolPage', document:GetPageCtrl())
+end
+
+function MySchool:Close()
+    local MySchoolPage = Store:Get('page/MySchoolPage')
+
+    if MySchoolPage then
+        MySchoolPage:CloseWindow()
+    end
+end
+
+function MySchool.GetUrl()
+    local token = Store:Get("user/token") or ''
+    return KeepworkService:GetKeepworkUrl() .. '/p/org/home?port=8099&token=' .. token
+end
--- Mod/WorldShare/config/Config.lua
+++ Mod/WorldShare/config/Config.lua
@@ -23,54 +23,54 @@ Config.defaultEnv = Config.env.ONLINE
 
 Config.keepworkList = {
   ONLINE = "https://keepwork.com",
-  STAGE = "https://stage.keepwork.com",
-  RELEASE = "https://release.keepwork.com",
-  LOCAL = "https://stage.keepwork.com"
+  STAGE = "http://dev.kp",
+  RELEASE = "http://rls.kp",
+  LOCAL = "http://dev.kp"
 }
 
 Config.keepworkServerList = {
   ONLINE = "https://api.keepwork.com/core/v0",
-  STAGE = "https://api-stage.keepwork.com/core/v0",
-  RELEASE = "https://api-release.keepwork.com/core/v0",
-  LOCAL = "https://api-stage.keepwork.com/core/v0",
+  STAGE = "https://api.dev.kp/core/v0",
+  RELEASE = "http://api.rls.kp/core/v0",
+  LOCAL = "http://api.dev.kp/core/v0",
 }
 
 Config.gitGatewayList = {
   ONLINE = "https://api.keepwork.com/git/v0",
-  STAGE = "https://api-stage.keepwork.com/git/v0",
-  RELEASE = "https://api-release.keepwork.com/git/v0",
-  LOCAL = "https://api-stage.keepwork.com/git/v0"
+  STAGE = "http://api.dev.kp/git/v0",
+  RELEASE = "http://api.rls.kp/git/v0",
+  LOCAL = "http://api.dev.kp/git/v0"
 }
 
 Config.esGatewayList = {
   ONLINE = "https://api.keepwork.com/es/v0",
-  STAGE = "https://api-stage.keepwork.com/es/v0",
-  RELEASE = "https://api-release.keepwork.com/es/v0",
-  LOCAL = "https://api-stage.keepwork.com/es/v0"
+  STAGE = "https://api.dev.kp/es/v0",
+  RELEASE = "https://api.rls.kp/es/v0",
+  LOCAL = "https://api.dev.kp/es/v0"
 }
 
 Config.lessonList = {
   ONLINE = "https://api.keepwork.com/lesson/v0",
-  STAGE = "https://api-stage.keepwork.com/lesson/v0",
-  RELEASE = "https://api-release.keepwork.com/lesson/v0",
-  LOCAL = "https://api-stage.keepwork.com/lesson/v0"
+  STAGE = "https://api.dev.kp/lesson/v0",
+  RELEASE = "https://api.rls.kp/lesson/v0",
+  LOCAL = "https://api.dev.kp/lesson/v0"
 }
 
 Config.dataSourceApiList = {
   gitlab = {
     ONLINE = "https://git.keepwork.com/api/v4",
-    STAGE = "https://git-stage.keepwork.com/api/v4",
-    RELEASE = "https://git-release.keepwork.com/api/v4",
-    LOCAL = "https://git-stage.keepwork.com/api/v4"
+    STAGE = "https://git.dev.kp/api/v4",
+    RELEASE = "https://git.rls.kp/api/v4",
+    LOCAL = "https://git.dev.kp/api/v4"
   }
 }
 
 Config.dataSourceRawList = {
   gitlab = {
     ONLINE = "https://git.keepwork.com",
-    STAGE = "https://git-stage.keepwork.com",
-    RELEASE = "https://git-release.keepwork.com",
-    LOCAL = "https://git-stage.keepwork.com"
+    STAGE = "https://git.dev.kp",
+    RELEASE = "https://git.rls.kp",
+    LOCAL = "https://git.dev.kp"
   }
 }
 
