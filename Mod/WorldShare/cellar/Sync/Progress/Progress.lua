de5ddbb5a0c25afcea69a19895c9cf7dc9178f63
onedou <onedou@126.com>
2020-06-22 12:06:18 +0800

    hotfix upload

--- Mod/WorldShare/cellar/Sync/Progress/Progress.lua
+++ Mod/WorldShare/cellar/Sync/Progress/Progress.lua
@@ -25,8 +25,19 @@ Progress.finish = false
 Progress.broke = false
 
 function Progress:Init(syncInstance)
-    local progressParams = Mod.WorldShare.Utils.ShowWindow(0, 0, "Mod/WorldShare/cellar/Sync/Progress/Progress.html", "Progress", 0, 0, "_fi", false, 9)
-    local operateParams = Mod.WorldShare.Utils.ShowWindow(270, 65, "Mod/WorldShare/cellar/Sync/Progress/Operate.html", "ProgressOperate", 230, -150 ,"_ct", false, 10)
+    local ProgressPage = Mod.WorldShare.Store:Get("page/Progress")
+    local ProgressOperate = Mod.WorldShare.Store:Get("page/ProgressOperate")
+
+    if ProgressPage then
+        ProgressPage:CloseWindow()
+    end
+
+    if ProgressOperatePage then
+        ProgressOperatePage:CloseWindow()
+    end
+
+    local progressParams = Mod.WorldShare.Utils.ShowWindow(0, 0, "Mod/WorldShare/cellar/Sync/Progress/Progress.html", "Progress", 0, 0, "_fi", false, 2)
+    local operateParams = Mod.WorldShare.Utils.ShowWindow(270, 65, "Mod/WorldShare/cellar/Sync/Progress/Operate.html", "ProgressOperate", 230, -150 ,"_ct", false, 3)
 
     if not progressParams._page or not operateParams._page then
         return false
@@ -93,7 +104,6 @@ function Progress:ClosePage()
 
     if ProgressPage then
         ProgressPage:CloseWindow()
-
         self.syncInstance:Close()
     end
 end
