4f6a811be64e3a16e8fd2d9370839e11c4b7868b
onedou <onedou@126.com>
2020-10-29 17:27:18 +0800

    fix bug

--- Mod/WorldShare/cellar/MySchool/MySchool.lua
+++ Mod/WorldShare/cellar/MySchool/MySchool.lua
@@ -29,7 +29,7 @@ function MySchool:Show(callback)
     KeepworkServiceSchoolAndOrg:GetUserAllOrgs(function(orgData)
         Mod.WorldShare.MsgBox:Close()
 
-        echo(orgData, true)
+        self.hasJoined = false
 
         if type(orgData) == "table" and #orgData > 0 then
             self.orgData = orgData
