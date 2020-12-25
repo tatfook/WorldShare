3361a1939dcd23e921934c27d0ae7ccff72b5f72
Pengbinbin <1627941175@qq.com>
2020-12-25 16:45:15 +0800

    update vip type name

--- Mod/WorldShare/cellar/VipNotice/VipNotice.lua
+++ Mod/WorldShare/cellar/VipNotice/VipNotice.lua
@@ -19,23 +19,23 @@ local UserInfo = NPL.load("(gl)Mod/WorldShare/cellar/UserConsole/UserInfo.lua")
 local VipNotice = NPL.export()
 
 VipNotice.TypeNames = {
-    a_school_management_system = L"此功能需求开通VIP",
-    t_online_teaching = L"此功能需求开通VIP",
-    s_online_learning = L"此功能需求开通VIP",
-    vip_world_data_save_as = L"此功能需求开通VIP",
-    vip_skin_of_all_protagonists = L"此功能需求开通VIP",
-    vip_python_code_block = L"此功能需求开通VIP",
-    vip_video_plugin_watermark_removal = L"此功能需求开通VIP",
-    vip_lan_40_people_online = L"此功能需求开通VIP",
-    vip_wan_networking = L"此功能需求开通VIP",
-    vip_online_world_data_50mb = L"此功能需求开通VIP",
-    MakeApp = L"此功能需求开通VIP",
-    ChangeAvatarSkin = L"此功能需求开通VIP",
-    t_create_vip_world = L"此功能需求开通VIP",
-    vip_code_game_art_of_war = L"此功能需求开通VIP",
-    vip_weekly_training = L"此功能需求开通VIP",
-    daily_note = L"此功能需求开通VIP",
-    fly_on_paraworld = L"此功能需求开通VIP",
+    a_school_management_system = L"马上开通会员激活功能",
+    t_online_teaching = L"马上开通会员激活功能",
+    s_online_learning = L"马上开通会员激活功能",
+    vip_world_data_save_as = L"马上开通会员激活功能",
+    vip_skin_of_all_protagonists = L"开通会员激活全部皮肤",
+    vip_python_code_block = L"开通会员使用Python方块",
+    vip_video_plugin_watermark_removal = L"开通会员去除视频水印",
+    vip_lan_40_people_online = L"开通会员扩展联网人数",
+    vip_wan_networking = L"开通会员组建互联网服务器",
+    vip_online_world_data_50mb = L"开通会员存储大型作品",
+    MakeApp = L"开通会员创建自己的App",
+    ChangeAvatarSkin = L"开通会员尽享精彩形象",
+    t_create_vip_world = L"开通会员创建特殊权限世界",
+    vip_code_game_art_of_war = L"开通会员畅享玩学课堂",
+    vip_weekly_training = L"马上开通会员激活功能",
+    daily_note = L"开通会员畅享每周实战",
+    fly_on_paraworld = L"开通会员马上飞行!",
 }
 VipNotice.onlyRecharge = false;
 
@@ -109,5 +109,5 @@ function VipNotice:GetQRCode()
 end
 
 function VipNotice:GetTypeName()
-    return VipNotice.TypeNames[VipNotice.from] or L"此功能需求开通VIP";
+    return VipNotice.TypeNames[VipNotice.from] or L"此功能需要开通会员";
 end
