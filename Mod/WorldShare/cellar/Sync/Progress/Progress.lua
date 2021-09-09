2d49ded6fb925c05e29facfd210d5b7bf2401229
onedou <onedou@126.com>
2021-09-09 09:44:21 +0800

    updated sync finish UI

--- Mod/WorldShare/cellar/Sync/Progress/Progress.lua
+++ Mod/WorldShare/cellar/Sync/Progress/Progress.lua
@@ -5,20 +5,20 @@ Date: 2017.4.17
 Desc: 
 use the lib:
 ------------------------------------------------------------
-local Progress = NPL.load("(gl)Mod/WorldShare/cellar/Sync/Progress/Progress.lua")
+local Progress = NPL.load('(gl)Mod/WorldShare/cellar/Sync/Progress/Progress.lua')
 ------------------------------------------------------------
 ]]
 
 -- UI
-local SyncMain = NPL.load("../Main.lua")
+local SyncMain = NPL.load('../Main.lua')
 
 -- service
-local Compare = NPL.load("(gl)Mod/WorldShare/service/SyncService/Compare.lua")
-local KeepworkServiceProject = NPL.load("(gl)Mod/WorldShare/service/KeepworkService/Project.lua")
-local KeepworkServiceSession = NPL.load("(gl)Mod/WorldShare/service/KeepworkService/Session.lua")
+local Compare = NPL.load('(gl)Mod/WorldShare/service/SyncService/Compare.lua')
+local KeepworkServiceProject = NPL.load('(gl)Mod/WorldShare/service/KeepworkService/Project.lua')
+local KeepworkServiceSession = NPL.load('(gl)Mod/WorldShare/service/KeepworkService/Session.lua')
 
 -- libs
-local Screen = commonlib.gettable("System.Windows.Screen")
+local Screen = commonlib.gettable('System.Windows.Screen')
 
 local Progress = NPL.export()
 
@@ -29,8 +29,8 @@ Progress.finish = false
 Progress.broke = false
 
 function Progress:Init(syncInstance)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Progress")
-    local ProgressOperate = Mod.WorldShare.Store:Get("page/ProgressOperate")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Progress')
+    local ProgressOperate = Mod.WorldShare.Store:Get('page/ProgressOperate')
 
     if ProgressPage then
         ProgressPage:CloseWindow()
@@ -40,8 +40,29 @@ function Progress:Init(syncInstance)
         ProgressOperatePage:CloseWindow()
     end
 
-    local progressParams = Mod.WorldShare.Utils.ShowWindow(0, 0, "Mod/WorldShare/cellar/Theme/Sync/Progress/Progress.html", "Mod.WorldShare.Progress", 0, 0, "_fi", false, 2)
-    local operateParams = Mod.WorldShare.Utils.ShowWindow(270, 65, "Mod/WorldShare/cellar/Theme/Sync/Progress/Operate.html", "Mod.WorldShare.Progress.ProgressOperate", 270, -150 ,"_ct", false, 3)
+    local progressParams = Mod.WorldShare.Utils.ShowWindow(
+        0,
+        0,
+        'Mod/WorldShare/cellar/Theme/Sync/Progress/Progress.html',
+        'Mod.WorldShare.Progress',
+        0,
+        0,
+        '_fi',
+        false,
+        2
+    )
+
+    local operateParams = Mod.WorldShare.Utils.ShowWindow(
+        270,
+        65,
+        'Mod/WorldShare/cellar/Theme/Sync/Progress/Operate.html',
+        'Mod.WorldShare.Progress.ProgressOperate',
+        270,
+        -150,
+        '_ct',
+        false,
+        3
+    )
 
     if not progressParams._page or not operateParams._page then
         return false
@@ -49,51 +70,58 @@ function Progress:Init(syncInstance)
 
     progressParams._page.OnClose = function()
         operateParams._page:CloseWindow()
-        Mod.WorldShare.Store:Remove("page/Mod.WorldShare.Progress")
-        Mod.WorldShare.Store:Remove("page/Mod.WorldShare.Progress.ProgressOperate")
+        Mod.WorldShare.Store:Remove('page/Mod.WorldShare.Progress')
+        Mod.WorldShare.Store:Remove('page/Mod.WorldShare.Progress.ProgressOperate')
     end
 
-    Mod.WorldShare.Store:Set("page/Progress", progressParams._page)
-    Mod.WorldShare.Store:Set("page/ProgressOperate", operateParams._page)
+    Mod.WorldShare.Store:Set('page/Progress', progressParams._page)
+    Mod.WorldShare.Store:Set('page/ProgressOperate', operateParams._page)
 
     self.syncInstance = syncInstance
 
     self.current = 0
     self.total = 0
-    self.msg = L"同步中，请稍候..."
+    self.msg = L'同步中，请稍候...'
     self.finish = false
     self.broke = false
 
     self:Refresh()
 
-    Screen:Connect("sizeChanged", self, self.OnScreenSizeChange, "UniqueConnection")
+    Screen:Connect('sizeChanged', self, self.OnScreenSizeChange, 'UniqueConnection')
 end
 
 function Progress:ShowFinishPage()
     if not KeepworkServiceSession:IsSignedIn() then
-        return false
+        return
     end
 
     local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld')
 
-    if not currentWorld or not currentWorld.kpProjectId or currentWorld.kpProjectId == 0 then
-        return false
+    if not currentWorld or
+       not currentWorld.kpProjectId or
+       currentWorld.kpProjectId == 0 then
+        return
     end
 
-    Mod.WorldShare.MsgBox:Show(L"请稍候...")
+    Mod.WorldShare.MsgBox:Wait()
 
     KeepworkServiceProject:GenerateMiniProgramCode(
         currentWorld.kpProjectId,
         function(bSucceed, wxacode)
             Mod.WorldShare.MsgBox:Close()
 
-            Mod.WorldShare.Utils.ShowWindow(550, 400, "Mod/WorldShare/cellar/Theme/Sync/Progress/Finish.html?wxacode=" .. (wxacode or ""), "Mod.WorldShare.Progress.Finish")
+            Mod.WorldShare.Utils.ShowWindow(
+                550,
+                400,
+                'Mod/WorldShare/cellar/Theme/Sync/Progress/Finish.html?wxacode=' .. (wxacode or ''),
+                'Mod.WorldShare.Progress.Finish'
+            )
         end
     )
 end
 
 function Progress:OnScreenSizeChange()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if ProgressPage then
         ProgressPage:Rebuild()
@@ -101,17 +129,17 @@ function Progress:OnScreenSizeChange()
 end
 
 function Progress:GetProgressBar()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
     end
 
-    return ProgressPage:GetNode("progressbar")
+    return ProgressPage:GetNode('progressbar')
 end
 
 function Progress:Refresh(delayTimeMs)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if ProgressPage then
         ProgressPage:Refresh(delayTimeMs or 0.01)
@@ -119,7 +147,7 @@ function Progress:Refresh(delayTimeMs)
 end
 
 function Progress:RefreshOperate()
-    local ProgressOperatePage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress.ProgressOperate")
+    local ProgressOperatePage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress.ProgressOperate')
 
     if ProgressOperatePage then
         ProgressOperatePage:Refresh(0.01)
@@ -127,7 +155,7 @@ function Progress:RefreshOperate()
 end
 
 function Progress:ClosePage()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if ProgressPage then
         ProgressPage:CloseWindow()
@@ -135,7 +163,7 @@ function Progress:ClosePage()
 end
 
 function Progress:CloseFinishPage()
-    local FinishPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress.Finish")
+    local FinishPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress.Finish')
 
     if FinishPage then
         FinishPage:CloseWindow()
@@ -147,7 +175,7 @@ function Progress:CloseFinishPage()
 end
 
 function Progress:Cancel(callback)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage or not self.syncInstance then
         return false
@@ -155,7 +183,7 @@ function Progress:Cancel(callback)
 
     self.syncInstance:SetBroke(true)
 
-    Mod.WorldShare.MsgBox:Show(L"正在等待上次同步完成，请稍候...", nil, nil, 380, 130, 11)
+    Mod.WorldShare.MsgBox:Show(L'正在等待上次同步完成，请稍候...', nil, nil, 380, 130, 11)
 
     self.broke = true
     self.finish = true
@@ -174,7 +202,7 @@ function Progress:Cancel(callback)
         Mod.WorldShare.MsgBox:Close()
         self:ClosePage()
 
-        if type(callback) == "function" then
+        if type(callback) == 'function' then
             callback()
         end
     end
@@ -187,7 +215,7 @@ function Progress:Retry()
         local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld')
         Compare:Init(currentWorld.worldpath, function(result)
             if not result then
-                GameLogic.AddBBS(nil, L"同步失败", 3000, "255 0 0")
+                GameLogic.AddBBS(nil, L'同步失败', 3000, '255 0 0')
                 return false
             end
 
@@ -207,7 +235,7 @@ function Progress:Retry()
 end
 
 function Progress:UpdateDataBar(current, total, msg)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -218,10 +246,10 @@ function Progress:UpdateDataBar(current, total, msg)
     self.msg = msg
 
     if not msg then
-        self.msg = L"同步中，请稍候..."
+        self.msg = L'同步中，请稍候...'
     end
 
-    LOG.std("Progress", "debug", "Progress", format("Totals : %s , Current : %s, Status : %s", self.total, self.current, self.msg))
+    LOG.std('Progress', 'debug', 'Progress', format('Totals : %s , Current : %s, Status : %s', self.total, self.current, self.msg))
 
     if self:GetFinish() then
         self:ClosePage()
@@ -236,7 +264,7 @@ function Progress:Copy(url)
 end
 
 function Progress:GetCurrent()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -246,7 +274,7 @@ function Progress:GetCurrent()
 end
 
 function Progress:GetTotal()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -256,7 +284,7 @@ function Progress:GetTotal()
 end
 
 function Progress:GetMsg()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -266,7 +294,7 @@ function Progress:GetMsg()
 end
 
 function Progress:SetFinish(value)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -276,7 +304,7 @@ function Progress:SetFinish(value)
 end
 
 function Progress:GetFinish()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -286,7 +314,7 @@ function Progress:GetFinish()
 end
 
 function Progress:SetBroke(value)
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
@@ -296,7 +324,7 @@ function Progress:SetBroke(value)
 end
 
 function Progress:GetBroke()
-    local ProgressPage = Mod.WorldShare.Store:Get("page/Mod.WorldShare.Progress")
+    local ProgressPage = Mod.WorldShare.Store:Get('page/Mod.WorldShare.Progress')
 
     if not ProgressPage then
         return false
--- Mod/WorldShare/command/LoadPersonalWorld.lua
+++ Mod/WorldShare/command/LoadPersonalWorld.lua
@@ -5,71 +5,84 @@ Date: 2020/9/25
 Desc: 
 use the lib:
 ------------------------------------------------------------
-local LoadPersonalWorldCommand = NPL.load("(gl)Mod/WorldShare/command/LoadPersonalWorld.lua")
+local LoadPersonalWorldCommand = NPL.load('(gl)Mod/WorldShare/command/LoadPersonalWorld.lua')
 -------------------------------------------------------
 ]]
 
 -- load lib
-local CmdParser = commonlib.gettable("MyCompany.Aries.Game.CmdParser")
-local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands")
-local CommandManager = commonlib.gettable("MyCompany.Aries.Game.CommandManager")
+local CmdParser = commonlib.gettable('MyCompany.Aries.Game.CmdParser')
+local Commands = commonlib.gettable('MyCompany.Aries.Game.Commands')
+local CommandManager = commonlib.gettable('MyCompany.Aries.Game.CommandManager')
+local WorldCommon = commonlib.gettable('MyCompany.Aries.Creator.WorldCommon')
 
 -- service
-local KeepworkServiceWorld = NPL.load("(gl)Mod/WorldShare/service/KeepworkService/World.lua")
-local KeepworkServiceProject = NPL.load("(gl)Mod/WorldShare/service/KeepworkService/Project.lua")
+local KeepworkServiceWorld = NPL.load('(gl)Mod/WorldShare/service/KeepworkService/World.lua')
+local KeepworkServiceProject = NPL.load('(gl)Mod/WorldShare/service/KeepworkService/Project.lua')
 
 -- UI
-local SyncMain = NPL.load("(gl)Mod/WorldShare/cellar/Sync/Main.lua")
-local WorldList = NPL.load("(gl)Mod/WorldShare/cellar/UserConsole/WorldList.lua")
-local LoginModal = NPL.load("(gl)Mod/WorldShare/cellar/LoginModal/LoginModal.lua")
+local SyncMain = NPL.load('(gl)Mod/WorldShare/cellar/Sync/Main.lua')
+local WorldList = NPL.load('(gl)Mod/WorldShare/cellar/UserConsole/WorldList.lua')
+local LoginModal = NPL.load('(gl)Mod/WorldShare/cellar/LoginModal/LoginModal.lua')
 
 local LoadPersonalWorldCommand = NPL.export()
 
 function LoadPersonalWorldCommand:Init()
     local loadpersonalworld = {
-        name="loadpersonalworld", 
-        quick_ref="/loadpersonalworld [project_id]", 
+        name='loadpersonalworld', 
+        quick_ref='/loadpersonalworld -nosync [project_id]', 
         desc=[[]],
-        mode_deny = "",
+        mode_deny = '',
         handler = function(cmd_name, cmd_text, cmd_params)
-            local options;
-            options, cmd_text = CmdParser.ParseOptions(cmd_text)
+            local option = ''
+            local kpProjectId = 0
+            local noSync = false
 
-            if cmd_params and cmd_params.value then
-                if type(cmd_params.value) ~= 'string' and type(cmd_params.value) ~= 'number' then
-                    return false
-                end
+            option, cmd_text = CmdParser.ParseOption(cmd_text)
 
-                local projectId = tonumber(cmd_params.value)
-
-                if not projectId then
-                    return false
-                end
-
-                LoginModal:CheckSignedIn("请先登录", function(bSucceed)
-                    KeepworkServiceProject:GetProject(projectId, function(data, err)
-                        if not data or type(data) ~= 'table' or not data.username then
-                            return false
-                        end
-    
-                        local username = Mod.WorldShare.Store:Get('user/username')
-                        
-                        if data.username ~= username then
-                            _guihelper.MessageBox(L"您正在试图加载的个人世界非您的个人世界，操作已被取消！")
-                            return
-                        end
-    
-                        WorldList:RefreshCurrentServerList(function()
-                            KeepworkServiceWorld:SetWorldInstanceByPid(projectId, function()
-                                SyncMain:SyncToLocal(function()
-                                    WorldList:EnterWorld()
-                                end, false)
-                            end)
-                        end)
-                    end)
-                end)
-
+            if option == 'nosync' then
+                noSync = true
             end
+
+            kpProjectId, cmd_text = CmdParser.ParseInt(cmd_text)
+
+            if not kpProjectId and type(kpProjectId) ~= 'number' then
+                return
+            end
+
+            LoginModal:CheckSignedIn('请先登录', function(bSucceed)
+                KeepworkServiceProject:GetProject(kpProjectId, function(data, err)
+                    if not data or type(data) ~= 'table' or not data.username then
+                        return false
+                    end
+
+                    local username = Mod.WorldShare.Store:Get('user/username')
+                    
+                    if data.username ~= username then
+                        _guihelper.MessageBox(L'您正在试图加载的个人世界非您的个人世界，操作已被取消！')
+                        return
+                    end
+
+                    if noSync then
+                        KeepworkServiceWorld:SetWorldInstanceByPid(kpProjectId, function()
+                            local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld')
+
+                            if currentWorld and currentWorld.worldpath then
+                                WorldCommon.OpenWorld(currentWorld.worldpath)
+                            end
+                        end)
+                    else
+                        KeepworkServiceWorld:SetWorldInstanceByPid(kpProjectId, function()
+                            SyncMain:SyncToLocal(function()
+                                local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld')
+
+                                if currentWorld and currentWorld.worldpath then
+                                    WorldCommon.OpenWorld(currentWorld.worldpath)
+                                end
+                            end, false)
+                        end)
+                    end
+                end)
+            end)
         end,
     }
 
