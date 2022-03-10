231937473a539918014cf36a8511bc553cb01e0b
yangguiyi <342949687@qq.com>
2022-03-10 16:18:58 +0800

    上报世界加载时长

--- Mod/WorldShare/cellar/Common/LoadWorld/CommonLoadWorld.lua
+++ Mod/WorldShare/cellar/Common/LoadWorld/CommonLoadWorld.lua
@@ -874,7 +874,8 @@ function CommonLoadWorld:EnterWorldById(pid, refreshMode, failed)
             -- stop here when overtime enter
             return
         end
-
+        -- 上报世界下载到加载耗时
+        GameLogic.GetFilters():apply_filters("user_behavior", 2, "duration.world_load", { started = true, projectId = worldInfo.projectId});
         LocalServiceHistory:LoadWorld({
             name = worldInfo.worldName,
             kpProjectId = worldInfo.projectId,
