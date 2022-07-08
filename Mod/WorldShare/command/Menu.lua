9eadec62ead4bc514c9686163020ef6b660f7c32
onedou <onedou@126.com>
2022-07-08 11:10:05 +0800

    fixed create new world

--- Mod/WorldShare/command/Menu.lua
+++ Mod/WorldShare/command/Menu.lua
@@ -5,28 +5,29 @@ Date: 2020/8/14
 Desc: 
 use the lib:
 ------------------------------------------------------------
-NPL.load("(gl)Mod/WorldShare/command/Menu.lua")
+NPL.load('(gl)Mod/WorldShare/command/Menu.lua')
 -------------------------------------------------------
 ]]
 
 -- load lib
-local CmdParser = commonlib.gettable("MyCompany.Aries.Game.CmdParser")
-local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands")
+local CmdParser = commonlib.gettable('MyCompany.Aries.Game.CmdParser')
+local Commands = commonlib.gettable('MyCompany.Aries.Game.Commands')
 
 -- service
-local KeepworkService = NPL.load("(gl)Mod/WorldShare/service/KeepworkService.lua")
-local EventTrackingService = NPL.load("(gl)Mod/WorldShare/service/EventTracking.lua")
+local KeepworkService = NPL.load('(gl)Mod/WorldShare/service/KeepworkService.lua')
+local EventTrackingService = NPL.load('(gl)Mod/WorldShare/service/EventTracking.lua')
 
 -- UI
-local ShareWorld = NPL.load("(gl)Mod/WorldShare/cellar/ShareWorld/ShareWorld.lua")
-local OpusSetting = NPL.load("(gl)Mod/WorldShare/cellar/OpusSetting/OpusSetting.lua")
-local MemberManager = NPL.load("(gl)Mod/WorldShare/cellar/MemberManager/MemberManager.lua")
+local ShareWorld = NPL.load('(gl)Mod/WorldShare/cellar/ShareWorld/ShareWorld.lua')
+local OpusSetting = NPL.load('(gl)Mod/WorldShare/cellar/OpusSetting/OpusSetting.lua')
+local MemberManager = NPL.load('(gl)Mod/WorldShare/cellar/MemberManager/MemberManager.lua')
+local CreateWorld = NPL.load('(gl)Mod/WorldShare/cellar/CreateWorld/CreateWorld.lua')
 
 local MenuCommand = NPL.export()
 
 function MenuCommand:Init()
-    NPL.load("(gl)script/apps/Aries/Creator/Game/Commands/CommandMenu.lua");
-    Commands["menu"].desc = Commands["menu"].desc .. [[
+    NPL.load('(gl)script/apps/Aries/Creator/Game/Commands/CommandMenu.lua');
+    Commands['menu'].desc = Commands['menu'].desc .. [[
 /menu project.share
 /menu project.index
 /menu project.author
@@ -38,39 +39,42 @@ end
 function MenuCommand:Call(cmdName, cmdText, cmdParams)
     local name, cmdText = CmdParser.ParseString(cmdText);
     if name and type(name) == 'string' then
-        local action = "click.menu." .. name
+        local action = 'click.menu.' .. name
 
         EventTrackingService:Send(1, action)
     end
 
-    if name == "project.share" then
+    if name == 'file.createworld' then
+        self:CreateWorld()
+        return true
+    elseif name == 'project.share' then
         self:Share()
         return true
-    elseif name == "project.index" then
+    elseif name == 'project.index' then
         self:OpenUserOpusPage()
         return true
-    elseif name == "project.author" then
+    elseif name == 'project.author' then
         self:OpenUserPage()
         return true
-    elseif name == "project.setting" then
+    elseif name == 'project.setting' then
         self:OpusSetting()
         return true
-    elseif name == "project.member" then
+    elseif name == 'project.member' then
         self:MemberManager()
         return true
-    elseif name == "project.apply" then
+    elseif name == 'project.apply' then
         self:Apply()
         return true
-    elseif name == "project.unfavorite" then
+    elseif name == 'project.unfavorite' then
         self:Favorite(true)
         return true
-    elseif name == "project.favorite" then
+    elseif name == 'project.favorite' then
         self:Favorite(false)
         return true
-    elseif name == "project.like" then
+    elseif name == 'project.like' then
         self:LikeProject()
         return true
-    elseif name == "project.unlike" then
+    elseif name == 'project.unlike' then
         return true
     end
 
@@ -82,32 +86,32 @@ function MenuCommand:Share()
 end
 
 GameLogic.GetFilters():add_filter(
-    "favorite_change",
+    'favorite_change',
     function (data)
         MenuCommand:ChangeFavoriteItemState(data.showFavorite)
     end
 )
 
 GameLogic.GetFilters():add_filter(
-    "like_change",
+    'like_change',
     function (data)
         MenuCommand:ChangeLikeItemState(data.isLiked)
     end
 )
 
 function MenuCommand:ChangeLikeItemState(isLiked)
-    NPL.load("(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenu.lua");
-    local DesktopMenu = commonlib.gettable("MyCompany.Aries.Creator.Game.Desktop.DesktopMenu")
-    local projectMenu = DesktopMenu.GetMenuItem("project")
+    NPL.load('(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenu.lua');
+    local DesktopMenu = commonlib.gettable('MyCompany.Aries.Creator.Game.Desktop.DesktopMenu')
+    local projectMenu = DesktopMenu.GetMenuItem('project')
     if projectMenu then
         for index, item in ipairs(projectMenu.children) do
-            if(item.Type ~= "Separator" ) then
-                if isLiked and item.name == "project.like" then
-                    item.name = "project.unlike"
-                    item.text = L"今日已点"
-                elseif not isLiked and item.name == "project.unlike"  then
-                    item.name = "project.like"
-                    item.text = L"点赞项目"
+            if(item.Type ~= 'Separator' ) then
+                if isLiked and item.name == 'project.like' then
+                    item.name = 'project.unlike'
+                    item.text = L'今日已点'
+                elseif not isLiked and item.name == 'project.unlike'  then
+                    item.name = 'project.like'
+                    item.text = L'点赞项目'
                 end
             end
         end
@@ -116,18 +120,18 @@ function MenuCommand:ChangeLikeItemState(isLiked)
 end
 
 function MenuCommand:ChangeFavoriteItemState(showFavorite)
-    NPL.load("(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenu.lua");
-    local DesktopMenu = commonlib.gettable("MyCompany.Aries.Creator.Game.Desktop.DesktopMenu")
-    local projectMenu = DesktopMenu.GetMenuItem("project")
+    NPL.load('(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenu.lua');
+    local DesktopMenu = commonlib.gettable('MyCompany.Aries.Creator.Game.Desktop.DesktopMenu')
+    local projectMenu = DesktopMenu.GetMenuItem('project')
     if projectMenu then
         for index, item in ipairs(projectMenu.children) do
-            if(item.Type ~= "Separator" ) then
-                if showFavorite and item.name == "project.unfavorite" then
-                    item.name = "project.favorite"
-                    item.text = L"收藏项目"
-                elseif not showFavorite and item.name == "project.favorite"  then
-                    item.name = "project.unfavorite"
-                    item.text = L"取消收藏"
+            if(item.Type ~= 'Separator' ) then
+                if showFavorite and item.name == 'project.unfavorite' then
+                    item.name = 'project.favorite'
+                    item.text = L'收藏项目'
+                elseif not showFavorite and item.name == 'project.favorite'  then
+                    item.name = 'project.unfavorite'
+                    item.text = L'取消收藏'
                 end
             end
         end
@@ -137,7 +141,7 @@ end
 
 function MenuCommand:LikeProject()
     if not GameLogic.GetFilters():apply_filters('is_signed_in') then
-		GameLogic.GetFilters():apply_filters('check_signed_in', "请先登录", function(result)
+		GameLogic.GetFilters():apply_filters('check_signed_in', '请先登录', function(result)
 			if result == true then
 				commonlib.TimerManager.SetTimeout(function()
 
@@ -148,8 +152,8 @@ function MenuCommand:LikeProject()
 		return
 	end
 
-    NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/keepwork.user.lua")
-    NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/keepwork.world.lua")
+    NPL.load('(gl)script/apps/Aries/Creator/HttpAPI/keepwork.user.lua')
+    NPL.load('(gl)script/apps/Aries/Creator/HttpAPI/keepwork.world.lua')
     local ProjectId = GameLogic.options:GetProjectId()
     if ProjectId then
         keepwork.world.star({router_params = {id = ProjectId}}, function(err, msg, data)
@@ -157,13 +161,13 @@ function MenuCommand:LikeProject()
             self:ChangeLikeItemState(isLiked)
         end)
     else
-        GameLogic.AddBBS(nil, L"点赞失败，请先分享该项目，再重试！", 3000, "255 0 0")
+        GameLogic.AddBBS(nil, L'点赞失败，请先分享该项目，再重试！', 3000, '255 0 0')
     end
 end
 
 function MenuCommand:Favorite(isFavorited)
     if not GameLogic.GetFilters():apply_filters('is_signed_in') then
-		GameLogic.GetFilters():apply_filters('check_signed_in', "请先登录", function(result)
+		GameLogic.GetFilters():apply_filters('check_signed_in', '请先登录', function(result)
 			if result == true then
 				commonlib.TimerManager.SetTimeout(function()
 
@@ -174,39 +178,39 @@ function MenuCommand:Favorite(isFavorited)
 		return
 	end
 
-    NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/keepwork.user.lua")
-    NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/keepwork.world.lua")
+    NPL.load('(gl)script/apps/Aries/Creator/HttpAPI/keepwork.user.lua')
+    NPL.load('(gl)script/apps/Aries/Creator/HttpAPI/keepwork.world.lua')
     local ProjectId = GameLogic.options:GetProjectId()
     if ProjectId then
         if isFavorited then
             keepwork.world.unfavorite({objectId = ProjectId, objectType = 5}, function(err, msg, data)
                 if (err == 200) then
-                    --GameLogic.AddBBS(nil, L"项目取消收藏成功", 3000, "0 255 0")
+                    --GameLogic.AddBBS(nil, L'项目取消收藏成功', 3000, '0 255 0')
                     self:ChangeFavoriteItemState(true)
                 elseif (err == 500) then
-                    GameLogic.AddBBS(nil, L"该项目已被其作者删除", 3000, "0 255 0")
+                    GameLogic.AddBBS(nil, L'该项目已被其作者删除', 3000, '0 255 0')
                 end
             end);
         else
             keepwork.world.favorite({objectId = ProjectId, objectType = 5}, function(err, msg, data)
                 if (err == 200) then
-                    --GameLogic.AddBBS(nil, L"项目收藏成功", 3000, "0 255 0")
+                    --GameLogic.AddBBS(nil, L'项目收藏成功', 3000, '0 255 0')
                     self:ChangeFavoriteItemState(false)
                 elseif (err == 500) then
-                    GameLogic.AddBBS(nil, L"该项目已被其作者删除", 3000, "0 255 0")
+                    GameLogic.AddBBS(nil, L'该项目已被其作者删除', 3000, '0 255 0')
                 else
-                    GameLogic.AddBBS(nil, L"收藏项目失败，请重试！", 3000, "0 255 0")
+                    GameLogic.AddBBS(nil, L'收藏项目失败，请重试！', 3000, '0 255 0')
                 end
             end)
         end
-        GameLogic.GetFilters():apply_filters("user_behavior", 1, "click.home.favorited")
+        GameLogic.GetFilters():apply_filters('user_behavior', 1, 'click.home.favorited')
     else
-        GameLogic.AddBBS(nil, L"收藏失败，请先分享该项目，再重试！", 3000, "255 0 0")
+        GameLogic.AddBBS(nil, L'收藏失败，请先分享该项目，再重试！', 3000, '255 0 0')
     end
 end
 
 function MenuCommand:OpenUserOpusPage()
-    ParaGlobal.ShellExecute("open", KeepworkService:GetShareUrl(), "", "", 1)
+    ParaGlobal.ShellExecute('open', KeepworkService:GetShareUrl(), '', '', 1)
 end
 
 function MenuCommand:OpenUserPage()
@@ -246,4 +250,8 @@ end
 
 function MenuCommand:Apply()
     MemberManager:ShowApply()
+end
+
+function MenuCommand:CreateWorld()
+    CreateWorld:CreateNewWorld()
 end
